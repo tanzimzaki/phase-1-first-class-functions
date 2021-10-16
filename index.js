3204aaca5e2a5f3c2dd2661aed80a524dd2f3c08
tanzimzaki <tanzimzaki@gmail.com>
2021-10-16 15:11:15 +0800

    done

---
+++ index.js
@@ -0,0 +1,9 @@
+const response = [Melissa, Aron, Laura, Krity];
+
+const spy = (callback) => {
+    
+    callback(response);
+}
+
+// basic callback functions by passing parameters using arrow functions - the module doesn not cover DOM (explained in later modules)
+// I beleive the functions module should have been covered once DOM module is completed 
